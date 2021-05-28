83db2df075124e9c0fb613ee2b066f8720da1fa4
ksi0730 <daisuke0730@gmail.com>
2021-05-28 23:14:35 +0900

    Add files via upload

---
+++ collective_action_firebase/env/firebase_init.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBr8YOyPE2eqFlFMy20LvMLC5Ybhssa6L4",
+    authDomain: "dev20chat-27d3f.firebaseapp.com",
+    databaseURL: "https://dev20chat-27d3f-default-rtdb.firebaseio.com/",
+    projectId: "dev20chat-27d3f",
+    storageBucket: "dev20chat-27d3f.appspot.com",
+    messagingSenderId: "284246193595",
+    appId: "1:284246193595:web:74c9bd1e3a36257803809e"
+  };
